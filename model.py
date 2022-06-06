8b94d4799790bfa748f54912db10a241f7c208c5
shubh25940000 <shubh2594@gmail.com>
2022-06-06 22:42:55 +0530

    Added requirement.txt

--- model.py
+++ model.py
@@ -20,6 +20,12 @@ lemma = WordNetLemmatizer()
 import warnings
 warnings.filterwarnings("ignore")
 pd.set_option("display.max_column", None)
+# nltk.download("all")
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 
 
